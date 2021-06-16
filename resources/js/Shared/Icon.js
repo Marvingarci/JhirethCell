a3e2304339dddab40305da58dea7f136d55050e5
Marvingarci <55658113+Marvingarci@users.noreply.github.com>
2021-06-16 13:39:02 -0600

    Update Icon.js

--- resources/js/Shared/Icon.js
+++ resources/js/Shared/Icon.js
@@ -119,25 +119,8 @@ export default ({ name, className }) => {
     );
   }
 
-  if (name === 'add-cart') {
-    return (
-      <svg
-        className={className}
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6"
-        fill="none"
-        viewBox="0 0 20 20"
-        stroke="currentColor"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-        />
-      </svg>
-    );
-  }
+  
+  
 
   if (name === 'store-front') {
     return (
